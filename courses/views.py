b900cadb2c7120155b015266cab87c3e2ed64f21
maufrais <maufrais@pasteur.fr>
2014-10-30 17:01:49 +0100

    tipo

--- courses/views.py
+++ courses/views.py
@@ -6,7 +6,7 @@ from models import Course
 class CourseView(TemplateView):
     template_name = "courses.html"
     model = Course
-    
+
     def get_context_data(self, *args, **kwargs):
         context = super(CourseView, self).get_context_data(*args, **kwargs)
         context['courses'] = Course.objects.all()
