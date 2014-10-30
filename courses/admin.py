e549eedf9554362d75c488ea62c0c76bd8f07ae1
maufrais <maufrais@pasteur.fr>
2014-10-30 16:55:29 +0100

    cations for courses

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django import forms
+from .models import Course, Teacher
 
 # Register your models here.
+class CourseAdminForm(forms.ModelForm):
+    class Meta:
+        model = Course
+
+
+class CourseAdmin(admin.ModelAdmin):
+    # list_display = ('__unicode__', )
+    # list_display = ('full_name', 'age' , 'profession')
+    # list_filter = ('age', 'profession')
+    form = CourseAdminForm
+
+
+class TeacherAdminForm(forms.ModelForm):
+    class Meta:
+        model = Teacher
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    # list_display = ('__unicode__', )
+    # list_display = ('full_name', 'age' , 'profession')
+    # list_filter = ('age', 'profession')
+    form = TeacherAdminForm
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Teacher, TeacherAdmin)
--- courses/views.py
+++ courses/views.py
@@ -3,10 +3,12 @@ from django.views.generic import TemplateView, DetailView
 from models import Course
 
 # Create your views here.
-
 class CourseView(TemplateView):
     template_name = "courses.html"
     model = Course
     
-
-    
+    def get_context_data(self, *args, **kwargs):
+        context = super(CourseView, self).get_context_data(*args, **kwargs)
+        context['courses'] = Course.objects.all()
+        # ajout de setting dans la conf generale
+        return context
