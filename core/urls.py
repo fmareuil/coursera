987b2ae01341e8c1d24bbac169e5a76805b795fb
fmareuil <fmareuil@pasteur.fr>
2014-10-30 14:22:28 +0100

    premier commit

---
+++ core/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from accueil import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'core.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$',views.HomeView.as_view(), name="accueil"),
+    url(r'^course/$',include("courses.urls"))
+)
---
+++ courses/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'core.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$',views.CourseView.as_view(), name="course"),
+)
