a1702cb216b139b4e892f0b9043f3478ca18f183
fabrice <fabrice.allain@pasteur.fr>
2014-10-30 17:08:44 +0100

    Sign in form view

--- accueil/views.py
+++ accueil/views.py
@@ -1,10 +1,13 @@
 #from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class HomeView(TemplateView):
     template_name = "accueil.html"
 
 
-class SignInView(TemplateView):
+class SignInView(FormView):
     template_name = "sign_in.html"
+    form_class = AuthenticationForm
+    success_url = '/'
